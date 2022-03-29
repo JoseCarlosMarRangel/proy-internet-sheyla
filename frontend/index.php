c3555124bf903c889b498372aa138301ad339835
Dinotator <56325558+Dinotator@users.noreply.github.com>
2022-03-29 10:16:15 -0600

    29/03/2022

---
+++ frontend/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/indexStyle.css">
+    <title>Menú</title>
+</head>
+<body>
+    <div class="contenedorPrincipal">
+        <div class="barraSuperior">
+            <div class="barraSuperiorUsuario">
+                <!--Aquí irá el usuario, de esto se encargarán los de backend ._.XD-->
+                *Imágen* *Nombre de usuario*
+            </div>
+        </div>
+
+        <div class="contenedorMedio">
+            <h1>BIENVENIDO @USER</h1>
+            <p>Departamento</p>
+        </div>
+        
+        <div class="contenedorInferior">
+            <div class="departamentos">
+                <div class="departamentoImagen">
+                    <br>
+                    <center>*Imagen*</center>
+                </div>  
+            </div>
+
+            <div class="departamentos">
+                <div class="departamentoImagen">
+                    <br>
+                    <center>*Imagen*</center>
+                </div>  
+            </div>
+
+            <div class="departamentos">
+                <div class="departamentoImagen">
+                    <br>
+                    <center>*Imagen*</center>
+                </div>  
+            </div>
+            
+            <div class="departamentos">
+                <div class="departamentoImagen">
+                    <br>
+                    <center>*Imagen*</center>
+                </div>  
+            </div>
+
+
+        </div>
+    </div>
+</body>
+</html>
