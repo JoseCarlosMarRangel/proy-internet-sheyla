9ccb5052a9a067ae4841fdc6099d3b737479496a
Dinotator <56325558+Dinotator@users.noreply.github.com>
2022-04-04 09:22:52 -0500

    Add files via upload

--- frontend/index.php
+++ frontend/index.php
@@ -5,52 +5,201 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/indexStyle.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <title>Menú</title>
 </head>
 <body>
     <div class="contenedorPrincipal">
         <div class="barraSuperior">
             <div class="barraSuperiorUsuario">
-                <!--Aquí irá el usuario, de esto se encargarán los de backend ._.XD-->
-                *Imágen* *Nombre de usuario*
+                <br>
+                *Foto de perfil* *Nombre*
             </div>
         </div>
 
         <div class="contenedorMedio">
+            <br><br>
             <h1>BIENVENIDO @USER</h1>
             <p>Departamento</p>
         </div>
         
         <div class="contenedorInferior">
-            <div class="departamentos">
-                <div class="departamentoImagen">
-                    <br>
-                    <center>*Imagen*</center>
-                </div>  
-            </div>
+            <br>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                            <div id="Carousel" class="carousel slide">
+                            <ol class="carousel-indicators">
+                                <li data-target="#Carousel" data-slide-to="0" class="active"></li>
+                                <li data-target="#Carousel" data-slide-to="1"></li>
+                                <li data-target="#Carousel" data-slide-to="2"></li>
+                            </ol>
+                             
+                            <!-- Carousel items -->
+                            <div class="carousel-inner">
+                                
+                            <div class="item active">
+                                <div class="row">
+                                  <div class="col-md-3">
+                                    <div class="departamentos">
+                                        <div class="departamentoImagen">
+                                            <br>
+                                            <center>*Imagen*</center>
+                                        </div>  
+                                        <br>
+                                        <p>Category Name | Date</p>
+                                        <h1>Departamento</h1>
+                                        <center>  
+                                        <button>Label</button>
+                                        </center>                                        
+                                    </div>
+                                  </div>
+                                  <div class="col-md-3">
+                                    <div class="departamentos">
+                                        <div class="departamentoImagen">
+                                            <br>
+                                            <center>*Imagen*</center>
+                                        </div>  
+                                        <br>
+                                        <p>Category Name | Date</p>
+                                        <h1>Departamento</h1>
+                                        <center>  
+                                        <button>Label</button>
+                                        </center>
 
-            <div class="departamentos">
-                <div class="departamentoImagen">
-                    <br>
-                    <center>*Imagen*</center>
-                </div>  
-            </div>
+                                    </div>
+                                  </div>
+                                  <div class="col-md-3">
+                                    <div class="departamentos">
+                                        <div class="departamentoImagen">
+                                            <br>
+                                            <center>*Imagen*</center>
+                                        </div>  
+                                        <br>
+                                        <p>Category Name | Date</p>
+                                        <h1>Departamento</h1>
+                                        
+                                        <center>  
+                                        <button>Label</button>
+                                        </center>
+                                    </div>
+                                  </div>
+                                  </div><!--.row-->
+                            </div><!--.item-->
+                             
+                            <div class="item">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <div class="departamentos">
+                                            <div class="departamentoImagen">
+                                                <br>
+                                                <center>*Imagen*</center>
+                                            </div>
+                                            <br>
+                                        <p>Category Name | Date</p>
+                                        <h1>Departamento</h1>  
+                                        
+                                        <center>  
+                                        <button>Label</button>
+                                        </center>
 
-            <div class="departamentos">
-                <div class="departamentoImagen">
-                    <br>
-                    <center>*Imagen*</center>
-                </div>  
-            </div>
-            
-            <div class="departamentos">
-                <div class="departamentoImagen">
-                    <br>
-                    <center>*Imagen*</center>
-                </div>  
-            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="departamentos">
+                                            <div class="departamentoImagen">
+                                                <br>
+                                                <center>*Imagen*</center>
+                                            </div>
+                                            <br>
+                                        <p>Category Name | Date</p>
+                                        <h1>Departamento</h1>
+                                        
+                                        <center>  
+                                        <button>Label</button>
+                                        </center>
+                                    </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="departamentos">
+                                            <div class="departamentoImagen">
+                                                <br>
+                                                <center>*Imagen*</center>
+                                            </div>
+                                            <br>
+                                        <p>Category Name | Date</p>
+                                        <h1>Departamento</h1>  
+                                        
+                                        <center>  
+                                        <button>Label</button>
+                                        </center>
 
+                                        </div>
+                                    </div>
+                                    </div><!--.row-->
+                            </div><!--.item-->
+                             
+                            <div class="item">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <div class="departamentos">
+                                            <div class="departamentoImagen">
+                                                <br>
+                                                <center>*Imagen*</center>
+                                            </div>  
+                                            <br>
+                                        <p>Category Name | Date</p>
+                                        <h1>Departamento</h1>
+                                        
+                                        <center>  
+                                        <button>Label</button>
+                                        </center>
 
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="departamentos">
+                                            <div class="departamentoImagen">
+                                                <br>
+                                                <center>*Imagen*</center>
+                                            </div>  
+                                            <br>
+                                        <p>Category Name | Date</p>
+                                        <h1>Departamento</h1>
+                                        
+                                        <center>  
+                                        <button>Label</button>
+                                        </center>
+
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="departamentos">
+                                            <div class="departamentoImagen">
+                                                <br>
+                                                <center>*Imagen*</center>
+                                            </div>
+                                            <br>
+                                        <p>Category Name | Date</p>
+                                        <h1>Departamento</h1>  
+                                        <center>  
+                                        <button>Label</button>
+                                        </center>
+                                        </div>
+                                    </div>
+                                </div><!--.row-->
+                            </div><!--.item-->
+                             
+                            </div><!--.carousel-inner-->
+                              <a data-slide="prev" href="#Carousel" class="left carousel-control">‹</a>
+                              <a data-slide="next" href="#Carousel" class="right carousel-control">›</a>
+                            </div><!--.Carousel-->
+                             
+                    </div>
+                </div>
+            </div><!--.container-->
         </div>
     </div>
 </body>
