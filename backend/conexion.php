85d128ad24c131e2cbf2be0394cecdcffec8d39f
Dinotator <56325558+Dinotator@users.noreply.github.com>
2022-04-17 00:36:11 -0500

    Vista Final

---
+++ backend/conexion.php
@@ -0,0 +1,87 @@
+<?php
+// * conexion a la base de datos
+
+$bd = 'upv_red';
+$servidor = 'localhost';
+$usuario = 'ghost'; //$usuario
+$contrasena = '123'; //$contrasena
+
+// * Creamos la conexion a la base de datos
+$conexion = mysqli_connect($servidor, $usuario, $contrasena, $bd);
+
+// * Checamos la conexion
+if (!$conexion) {
+    die('Conexión a la base de datos ' . $bd . ' fallida: ' .
+        mysqli_connect_error());
+}
+
+function valida_usuario_bd($usuario, $contrasena, $conexion)
+{
+    $query = "SELECT * FROM empleados WHERE Usuario = '$usuario' AND pass = '$contrasena'";
+    //echo $query;
+    $resultado = mysqli_query($conexion, $query) or die(mysqli_error($conexion));
+    $filas = mysqli_num_rows($resultado);
+    if (mysqli_num_rows($resultado) == 0) {
+        return false;
+        echo "Usuario o contraseña incorrectos";
+    } else {
+        return true;
+    }
+};
+
+function registrar_usuario_bd($nombre, $apellido, $correo, $usuario, $password, $departamento, $conexion)
+{
+    echo "nombre: " . $nombre . " apellido: " . $apellido . " correo: " . $correo . " usuario: " . $usuario . " password: " . $password . " departamento " . $departamento;
+    $query = "INSERT INTO empleados (ID_empleado,ID_departamento,Nombre_empleado,Apellido_empleado,Correo,Usuario,pass,rol) 
+    VALUES (DEFAULT,$departamento,'$nombre','$apellido','$correo','$usuario','$password',1)";
+    if (mysqli_query($conexion, $query)) {
+        echo "Registro exitoso";
+    } else {
+        echo "Error: " . $query . "<br>" . mysqli_error($conexion);
+    }
+}
+
+function modificar_empleado($nombre, $apellido, $correo, $usuario, $password, $departamento, $conexion, $id)
+{
+    $query = "UPDATE empleados SET Nombre_empleado = '$nombre',
+     Apellido_empleado = '$apellido', Correo = '$correo',
+      Usuario = '$usuario', pass = '$password', ID_departamento = '$departamento'
+       WHERE ID_empleado = '$id'";
+    if (mysqli_query($conexion, $query)) {
+        echo "Modificacion exitosa";
+    } else {
+        echo "Error: " . $query . "<br>" . mysqli_error($conexion);
+    }
+}
+
+function borrar_empleados($conexion, $usuario)
+{
+    echo "Borrar empleados";
+    $query = "DELETE FROM empleados WHERE usuario = '$usuario'";
+    if (mysqli_query($conexion, $query)) {
+        echo "borrado exitoso";
+    } else {
+        echo "Error: " . $query . "<br>" . mysqli_error($conexion);
+    }
+}
+
+function registrar_citas($nombre, $telefono, $descripcion, $fecha, $conexion)
+{
+    $query = "INSERT INTO citas (ID_cita,Nombre_cliente,Telefono,Descripcion,Fechas) 
+    VALUES (DEFAULT,'$nombre',$telefono,'$descripcion','$fecha')";
+    if (mysqli_query($conexion, $query)) {
+        echo "Registro exitoso";
+    } else {
+        echo "Error: " . $query . "<br>" . mysqli_error($conexion);
+    }
+}
+
+function borrar_citas($id, $conexion)
+{
+    $query = "DELETE FROM citas WHERE id_cita = $id";
+    if (mysqli_query($conexion, $query)) {
+        echo "Borrado exitoso exitoso";
+    } else {
+        echo "Error: " . $query . "br" . mysqli_error($conexion);
+    }
+}
--- upv_red.sql
+++ upv_red.sql
@@ -1,56 +1,20 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 28-03-2022 a las 18:26:58
--- Versión del servidor: 10.4.21-MariaDB
--- Versión de PHP: 8.0.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `upv_red`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `citas`
---
-
 CREATE TABLE `citas` (
   `Id_cita` int(11) NOT NULL,
-  `Nombre_cliente` varchar(30) NOT NULL,
-  `Telefono` int(11) NOT NULL,
-  `Descripcion` varchar(50) NOT NULL,
+  `title` varchar(30) NOT NULL,
+  `Telefono` bigint(30) NOT NULL,
+  `Description` varchar(50) NOT NULL,
   `Fechas` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `departamentos`
---
 
 CREATE TABLE `departamentos` (
   `ID_departamento` int(11) NOT NULL,
   `Nombre_departamento` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `documento`
---
 
 CREATE TABLE `documento` (
   `ID_documento` int(11) NOT NULL,
@@ -61,12 +25,6 @@ CREATE TABLE `documento` (
   `Estado` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `empleados`
---
-
 CREATE TABLE `empleados` (
   `ID_empleado` int(11) NOT NULL,
   `ID_departamento` int(11) NOT NULL,
@@ -74,14 +32,16 @@ CREATE TABLE `empleados` (
   `Apellido_empleado` varchar(30) NOT NULL,
   `Correo` varchar(40) NOT NULL,
   `Usuario` varchar(30) NOT NULL,
-  `Password` varchar(32) NOT NULL
+  `pass` varchar(32) NOT NULL,
+  `rol` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `inventario`
---
+CREATE TABLE `eventos` (
+  `id_evento` int(11) NOT NULL,
+  `nombre_evento` varchar(30) NOT NULL,
+  `fecha` date NOT NULL,
+  `Descripcion` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `inventario` (
   `ID_inventario` int(11) NOT NULL,
@@ -90,93 +50,61 @@ CREATE TABLE `inventario` (
   `Descripcion` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `citas`
---
 ALTER TABLE `citas`
   ADD PRIMARY KEY (`Id_cita`);
 
---
--- Indices de la tabla `departamentos`
---
 ALTER TABLE `departamentos`
   ADD PRIMARY KEY (`ID_departamento`);
 
---
--- Indices de la tabla `documento`
---
 ALTER TABLE `documento`
   ADD PRIMARY KEY (`ID_documento`),
   ADD KEY `ID_departamento` (`ID_departamento`);
 
---
--- Indices de la tabla `empleados`
---
 ALTER TABLE `empleados`
   ADD PRIMARY KEY (`ID_empleado`),
   ADD KEY `dept` (`ID_departamento`);
 
---
--- Indices de la tabla `inventario`
---
 ALTER TABLE `inventario`
   ADD PRIMARY KEY (`ID_inventario`);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `citas`
---
 ALTER TABLE `citas`
-  MODIFY `Id_cita` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id_cita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- AUTO_INCREMENT de la tabla `departamentos`
---
 ALTER TABLE `departamentos`
-  MODIFY `ID_departamento` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_departamento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
---
--- AUTO_INCREMENT de la tabla `documento`
---
 ALTER TABLE `documento`
   MODIFY `ID_documento` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `empleados`
---
 ALTER TABLE `empleados`
-  MODIFY `ID_empleado` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID_empleado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
---
--- AUTO_INCREMENT de la tabla `inventario`
---
 ALTER TABLE `inventario`
   MODIFY `ID_inventario` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `documento`
---
 ALTER TABLE `documento`
   ADD CONSTRAINT `documento_ibfk_1` FOREIGN KEY (`ID_departamento`) REFERENCES `departamentos` (`ID_departamento`);
 
---
--- Filtros para la tabla `empleados`
---
 ALTER TABLE `empleados`
   ADD CONSTRAINT `dept` FOREIGN KEY (`ID_departamento`) REFERENCES `departamentos` (`ID_departamento`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO `departamentos` (`ID_departamento`, `Nombre_departamento`) VALUES
+(1, 'Direccion general'),
+(2, 'Direccion'),
+(3, 'Recursos Humanos'),
+(4, 'Administracion'),
+(5, 'Comercial'),
+(6, 'Legal'),
+(7, 'Produccion');
+
+INSERT INTO `empleados` (`ID_empleado`, `ID_departamento`, `Nombre_empleado`, `Apellido_empleado`, `Correo`, `Usuario`, `pass`, `rol`) VALUES
+(1, 1, 'Alfonso Aldahir ', 'Hernandez Rodriguez', 'AAHernandezR@upred.com', 'DGAlfonso', 'dg1', 0),
+(2, 1, 'Adriana', 'Palmero Torres', 'APalemeroT@upred.com', 'DGPalemero', 'dg2', 0),
+(3, 2, 'Gustavo Angel', 'Vargas Pesina', 'GAVargasP@upred.com', 'DGustavo', 'd1', 0),
+(4, 2, 'Marco Alejandro', 'Hernández Castellanos', 'MAHernandezC@upred.com', 'DMarco', 'd2', 0),
+(9, 5, 'José Carlos', 'Mar Rangel', 'JCMarR@upred.com', 'CJose', 'c1', 0),
+(10, 5, 'Cristal Elizabeth', 'Toscano Hernández', 'CEToscanoH@upred.com', 'CCristal', 'c2', 0);
+
+INSERT INTO `citas` (`Id_cita`, `title`, `Telefono`, `Description`, `Fechas`) VALUES
+(1, 'carl', 8342666114, 'soy carlos', '2022-04-13');
